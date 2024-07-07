fab557ae24d406428a4eef0804210b29b230ecf1
Bhavy Patel <94931828+bhavycan@users.noreply.github.com>
2024-07-07 16:11:03 -0600

    changes on text

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 gsap.registerPlugin(ScrollTrigger);
 
 // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
@@ -24,30 +22,11 @@ ScrollTrigger.scrollerProxy("#main", {
 });
 
 
-
-
-
 // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
